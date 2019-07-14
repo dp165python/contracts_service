e7476ef0b067fd11bb3bd55b97cad2bcfc241593
maksym-bielyshev <maksym.bielyshev@gmail.com>
2019-07-14 21:29:43 +0300

    new format for test data

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,77 +1,39 @@
-all_contracts = {
-    "Contracts": [
-        {
-            "contract_name": "Contract_1",
-            "information": "message",
-            "url": "http://localhost:5000/contracts/86c22578-e914-43f8-9125-032bd7a4419a",
-            "rule": {
-                "f_operand": "field_1",
-                "operator": "+",
-                "url": "http://localhost:5000/contracts/rules/b45de920-c55f-4782-8a7a-cdf72fe438f6",
-                "coefficient": "coef",
-                "name": "Rule_1",
-                "creation_date": "2019-07-03T19:55:35.508901+00:00",
-                "s_operand": "field_2",
-                "id": "b45de920-c55f-4782-8a7a-cdf72fe438f6"
-            },
-            "creation_date": "2019-07-03T19:55:44.693399+00:00",
-            "id": "86c22578-e914-43f8-9125-032bd7a4419a"
-        },
-        {
-            "contract_name": "Contract_3",
-            "information": "message",
-            "url": "http://localhost:5000/contracts/bd6e148d-254e-465f-b7bb-5fc7231c358a",
-            "rule": {
-                "f_operand": "field_1",
-                "operator": "+",
-                "url": "http://localhost:5000/contracts/rules/b45de920-c55f-4782-8a7a-cdf72fe438f6",
-                "coefficient": "coef",
-                "name": "Rule_1",
-                "creation_date": "2019-07-03T19:55:35.508901+00:00",
-                "s_operand": "field_2",
-                "id": "b45de920-c55f-4782-8a7a-cdf72fe438f6"
-            },
-            "creation_date": "2019-07-03T19:56:58.445296+00:00",
-            "id": "bd6e148d-254e-465f-b7bb-5fc7231c358a"
-        },
+all_contracts = \
+[
+    {
+        "rules": [
+            {
+                "id": "c159aa4e-d9c2-464c-a2e7-c89308874918",
+                "s_operand": "field_number_2",
+                "f_operand": "field_number_1",
+                "coefficient": "coef_1",
+                "rule_name": "Rule_3",
+                "operator": "+"
+            }
+        ],
+        "name": "Contract_1",
+        "id": "6f3b6904-7135-40d0-8437-832992bc472e",
+        "information": "information"
+    },
+    {
+        "rules": [
+            {
+                "id": "0e2a6456-a364-4917-9f4a-6b8cb3fd3f9e",
+                "s_operand": "field_number_2",
+                "f_operand": "field_number_1",
+                "coefficient": "coef_1",
+                "rule_name": "Rule_0",
+                "operator": "+"
+            }
+        ],
+        "name": "Contract_2",
+        "id": "65712551-5a61-415d-93fd-bfd91c259364",
+        "information": "information"
+    }
+]
 
-        {
-            "contract_name": "Contract_4",
-            "information": "message",
-            "url": "http://localhost:5000/contracts/9150ff7a-a354-4e3b-b1b7-550974dd2fb1",
-            "rule": {
-                "f_operand": "field_number",
-                "operator": "+",
-                "url": "http://localhost:5000/contracts/rules/52343fdd-0a0d-4a5d-861f-db0cdd101554",
-                "coefficient": "coef",
-                "name": "Rule_4",
-                "creation_date": "2019-07-03T19:57:29.887045+00:00",
-                "s_operand": "field_number",
-                "id": "52343fdd-0a0d-4a5d-861f-db0cdd101554"
-            },
-            "creation_date": "2019-07-03T19:57:29.887045+00:00",
-            "id": "9150ff7a-a354-4e3b-b1b7-550974dd2fb1"
-        }
-    ]
-}
-
-contract = {
-    "Contract":
-        {
-            "contract_name": "Contract_1",
-            "information": "message",
-            "url": "http://localhost:5000/contracts/86c22578-e914-43f8-9125-032bd7a4419a",
-            "rule": {
-                "f_operand": "field_1",
-                "operator": "+",
-                "url": "http://localhost:5000/contracts/rules/b45de920-c55f-4782-8a7a-cdf72fe438f6",
-                "coefficient": "coef",
-                "name": "Rule_1",
-                "creation_date": "2019-07-03T19:55:35.508901+00:00",
-                "s_operand": "field_2",
-                "id": "b45de920-c55f-4782-8a7a-cdf72fe438f6"
-            },
-            "creation_date": "2019-07-03T19:55:44.693399+00:00",
-            "id": "86c22578-e914-43f8-9125-032bd7a4419a"
-        }
+contract = \
+{
+    "name": "test_contract",
+    "information": "test_information"
 }
