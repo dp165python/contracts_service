a918fde32b0977f538f15d958b83708ae4342f72
maksym-bielyshev <maksym.bielyshev@gmail.com>
2019-07-21 23:04:14 +0300

    fixed config to tests

--- core/config.py
+++ core/config.py
@@ -13,9 +13,9 @@ migrate = Migrate()
 class Config:
     DB_USER = os.environ.get('DB_USER', 'postgres')
     DB_PASSWORD = os.environ.get('DB_PASSWORD', '1')
-    DEFAULT_DB = os.environ.get('DEFAULT_DB', 'contracts')
     DB_HOST = os.environ.get('DB_HOST', 'localhost')
     DB_PORT = os.environ.get('DB_PORT', 5432)
+    DEFAULT_DB = os.environ.get('DEFAULT_DB', 'contracts')
     DEBUG = False
     HOST = '127.0.0.1'
     LOG_LEVEL = logging.INFO
@@ -33,11 +33,17 @@ class DevConfig(Config):
 
 
 class TestConfig(Config):
+    DB_USER = os.environ.get('DB_USER', 'postgres')
+    DB_PASSWORD = os.environ.get('DB_PASSWORD', '1')
+    DB_HOST = os.environ.get('DB_HOST', 'localhost')
+    DB_PORT = os.environ.get('DB_PORT', 5432)
     DEFAULT_DB = os.environ.get('DEFAULT_DB', 'test_contracts')
+    TOKEN = os.environ.get('TOKEN', None)
+    SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DEFAULT_DB}'
 
 
 def runtime_config():
-    env = os.environ.get("APP_ENV", APP_ENV_DEV).strip().lower()
+    env = os.environ.get('APP_ENV', APP_ENV_TEST).strip().lower()
     if env == APP_ENV_PROD:
         return ProdConfig
     elif env == APP_ENV_TEST:
